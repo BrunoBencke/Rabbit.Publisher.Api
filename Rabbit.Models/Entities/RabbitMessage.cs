062d29f54211428da723d65e789b1c20d5dabbf5
BrunoBencke <bruno.bencke@tramontina.com>
2023-05-09 18:01:07 -0300

    First commit

---
+++ Rabbit.Models/Entities/RabbitMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.Models.Entities
+{
+    public class RabbitMessage
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+    }
+}
---
+++ Rabbit.Services/RabbitMessageService.cs
@@ -0,0 +1,20 @@
+﻿using Rabbit.Models.Entities;
+using Rabbit.Repositories.Interfaces;
+using Rabbit.Services.Interfaces;
+
+namespace Rabbit.Services
+{
+    public class RabbitMessageService : IRabbitMessageService
+    {
+        private readonly IRabbitMessageRepository _repostiory;
+
+        public RabbitMessageService(IRabbitMessageRepository repository) {
+            _repostiory = repository;
+        }
+
+        public void SendMessage(RabbitMessage message)
+        {
+            _repostiory.SendMessage(message);
+        }
+    }
+}
